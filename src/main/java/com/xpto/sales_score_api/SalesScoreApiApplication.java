fe0e14aa86e7cbc4cf642ed9ff72a5429c4e489c
ericmariot <eric.mariot@betrybe.com>
2024-10-02 18:13:32 -0300

    feat: initial commit

---
+++ src/main/java/com/xpto/sales_score_api/SalesScoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.xpto.sales_score_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesScoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesScoreApiApplication.class, args);
+	}
+
+}
